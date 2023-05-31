8d06793ff87fafc52d2b61f991532997b0f30c64
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-01 01:54:14 +0330

    root

--- GUI/main.py
+++ GUI/main.py
@@ -1,1 +1,19 @@
 ## GUI 403.online service ##
+
+# packgae
+import requests
+from customtkinter import *
+from colorama import Fore as c
+
+
+main():
+    root = CTk()
+
+    root.mainloop()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
