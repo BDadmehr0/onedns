083c71d9b3d705c34b47338fea962da2f060ee38
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-08-11 19:41:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_path=$(pwd)
-tntsms_content="#!/bin/bash\n\ncd $script_path\n./d403.sh"
+tntsms_content="#!/bin/bash\n\ncd $script_path\n./onedns.sh"
 
-echo -e $tntsms_content > /usr/local/bin/d403.sh
-chmod +x /usr/local/bin/d403.sh
+echo -e $tntsms_content > /usr/local/bin/onedns.sh
+chmod +x /usr/local/bin/onedns.sh
