dc3f56c90dbdb0717926102699de6ed3b1df2c50
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-08-11 20:39:04 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-script_path=$(pwd)
-tntsms_content="#!/bin/bash\n\ncd $script_path\n./onedns.sh"
-
-echo -e $tntsms_content > /usr/local/bin/onedns.sh
-chmod +x /usr/local/bin/onedns.sh
