fd0b35ffc58650f9fab54ffa8be0fd9c50b913d0
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-01-21 15:14:35 +0000

    Update onedns_installer.sh
    https://github.com/BDadmehr0/onedns/issues/3

--- onedns_installer.sh
+++ onedns_installer.sh
@@ -7,124 +7,88 @@ YELLOW=$(tput setaf 3)
 BLUE=$(tput setaf 4)
 WHITE=$(tput sgr0)
 
-# Service Check API
-url='https://api.anti403.ir/api/search-filter?url='
-
-fil_and_sop='{"isSuccess":true,"message":"result success","result":{"support":true,"sanction_status":true,"crossings":[{"name":"DNS","link":"\/download"},{"name":"\u0633\u0631\u0648\u06cc\u0633 \u0648\u06cc\u0698\u0647","link":"\/subscription"}]},"statusCode":200}'
-n_fil='{"isSuccess":true,"message":"result success","result":{"support":false,"sanction_status":false,"crossings":null},"statusCode":200}'
-fill_and_n_sop='{"isSuccess":true,"message":"result success","result":{"support":false,"sanction_status":true,"crossings":null},"statusCode":200}'
-
-
-
-# DNS configurations
-electro="nameserver 78.157.42.101\nnameserver 78.157.42.100"
-shecandns="nameserver 178.22.122.100\nnameserver 185.51.200.2"
-text="nameserver 10.202.10.202\nnameserver 10.202.10.102"
-reset_text="nameserver 8.8.8.8"
-
 # Banner
 banner="${BLUE}
   ___             ____  _   _ ____  
- / _ \ _ __   ___|  _ \| \ | / ___| 
-| | | | '_ \ / _ \ | | |  \| \___ \ 
-| |_| | | | |  __/ |_| | |\  |___) | | github BDadmehr0/onedns
- \___/|_| |_|\___|____/|_| \_|____/  | V.2${WHITE}\n"
+ / _ \\ _ __   ___|  _ \\| \\ | / ___| 
+| | | | '_ \\ / _ \\ | | |  \\| \\___ \\ 
+| |_| | | | |  __/ |_| | |\\  |___) | | github BDadmehr0/onedns
+ \\___/|_| |_|\\___|____/|_| \\_|____/  | V.2${WHITE}\n"
+
+# Menu Banner
+menu_banner="${BLUE}[${WHITE}1${BLUE}]${WHITE} Start\n${BLUE}[${WHITE}2${BLUE}]${WHITE} Reset DNS to (8.8.8.8)\n${BLUE}[${WHITE}3${BLUE}]${WHITE} Custom DNS\n${BLUE}[${WHITE}4${BLUE}]${WHITE} Service check\n${BLUE}[${WHITE}5${BLUE}]${WHITE} About\n${BLUE}[${WHITE}6${BLUE}]${WHITE} Show my DNS\n${BLUE}[${WHITE}00${BLUE}]${WHITE} Exit${WHITE}\n"
+
+# Function to validate IP addresses
+validate_ip() {
+    local ip="$1"
+    local valid=1
+    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+        for octet in $(echo "$ip" | tr '.' ' '); do
+            if ((octet < 0 || octet > 255)); then
+                valid=0
+                break
+            fi
+        done
+    else
+        valid=0
+    fi
+    return $valid
+}
 
 # Check if running as root
-if [ "$(whoami)" = "root" ]; then
-    echo -e "$banner"
-
-	start_menu="${BLUE}[${WHITE}1${BLUE}]${WHITE} 403 online dns\n${BLUE}[${WHITE}2${BLUE}]${WHITE} Electro (for youtube unlock)\n${BLUE}[${WHITE}3${BLUE}]${WHITE} Shecan dns\n${BLUE}[${WHITE}00${BLUE}]${WHITE} Exit\n"
-
-    # Menu Banner
-    mode_banner="${BLUE}[${WHITE}1${BLUE}]${WHITE} Start\n${BLUE}[${WHITE}2${BLUE}]${WHITE} Reset DNS to (8.8.8.8)\n${BLUE}${BLUE}[${WHITE}3${BLUE}]${WHITE} Service check\n${BLUE}[${WHITE}4${BLUE}]${WHITE} New Service request(Cooming)\n${WHITE}${BLUE}[${WHITE}5${BLUE}]${WHITE} About\n${BLUE}[${WHITE}6${BLUE}]${WHITE} Show my DNS\n${BLUE}[${WHITE}00${BLUE}]${WHITE} Exit${WHITE}\n"
-    echo -e "$mode_banner"
-
-    # Menu loop
-    while true; do
-        read -p "Select: " i
-        echo " "
-        case $i in
-            1)
-				clear
-				echo -e "$banner"
-				while true; do
-					echo -e "$start_menu"
-       				read -p "Select: " i
-					case $i in
-						1)
-                			echo -e "$text" > /etc/resolv.conf
-                			echo -e "\n${GREEN}Change DNS to (10.202.10.202, 10.202.10.102)${WHITE}\n"
-                			exit
-							;;
-						3)
-							echo -e "$shecandns" > /etc/resolv.conf
-                			echo -e "\n${GREEN}Change DNS to (178.22.122.100, 185.51.200.2)${WHITE}\n"
-                			exit
-							;;
-						2)
-							echo -e "$electro" > /etc/resolv.conf
-							echo -e "\n${GREEN}Change DNS to (78.157.42.101, 78.157.42.100)${WHITE}\n"
-							exit
-							;;
-						00)
-							exit
-					echo -e "\n${YELLOW}Bye bye${WHITE}\n"
-					exit
-							;;
-					esac
-				done
-                ;;
-            2)
-                echo -e "$reset_text" > /etc/resolv.conf
-				echo -e "${GREEN}Change DNS to (8.8.8.8)${WHITE}\n"
-                exit
-                ;;
-            3)
-                read -p "Service Url: " service_s_url
-                response=$(curl -s "${url}${service_s_url}")
-                case "$response" in
-                    "$fil_and_sop")
-                        echo -e "\n${YELLOW}URL Filter or support 403 Service${WHITE}\n"
-                        ;;
-                    "$n_fil")
-                        echo -e "\n${GREEN}URL No Filter or support 403 Service${WHITE}\n"
-                        ;;
-                    "$fill_and_n_sop")
-                        echo -e "\n${RED}URL Filter or not support 403 Service${WHITE}\n"
-                        ;;
-                esac
-                ;;
-            4)
-                # s
-                ;;
-            5)
-            echo -e "One DNS is made up of several DNS + separate tools to improve the quality of DNS.\nThe good thing about this DNS is that it can be easily installed and executed on Linux systems and automatically adjusts DNS."                ;;
-            6)
-
-                #echo res.confg
-                command="cat /etc/resolv.conf"
-                rep1="options edns0 trust-ad"
-
-                # Run
-                out=$(eval "$command")
-                echo -e "${GREEN}$out${WHITE}\n"
-                ;;
-            00)
-                #EXIT
-                exit
-                ;;
-            clear)
-                clear
-                echo -e "$banner"
-                echo -e "$mode_banner"
-                ;;
-            *)
-                echo -e "${RED}Command Not Found${WHITE}\n"
-                exit
-                ;;
-        esac
-    done
-else
-    echo -e "\n${RED}Please run with sudo for edit /etc/resolv.conf\n${WHITE}"
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo -e "\n${RED}Please run with sudo for editing /etc/resolv.conf${WHITE}\n"
+    exit 1
 fi
+
+# Display banner
+clear
+echo -e "$banner"
+echo -e "$menu_banner"
+
+# Menu loop
+while true; do
+    read -p "Select: " choice
+    echo ""
+    case $choice in
+        1)
+            echo -e "\n${YELLOW}Start: DNS Management Options Coming Soon${WHITE}\n"
+            ;;
+        2)
+            echo -e "nameserver 8.8.8.8" > /etc/resolv.conf
+            echo -e "\n${GREEN}DNS reset to 8.8.8.8${WHITE}\n"
+            ;;
+        3) # Custom DNS
+            read -p "Enter primary DNS: " primary_dns
+            if ! validate_ip "$primary_dns"; then
+                echo -e "${RED}Invalid DNS address${WHITE}\n"
+                continue
+            fi
+            read -p "Enter secondary DNS (optional): " secondary_dns
+            if [[ -n "$secondary_dns" && ! $(validate_ip "$secondary_dns") ]]; then
+                echo -e "${RED}Invalid secondary DNS address${WHITE}\n"
+                continue
+            fi
+            echo -e "nameserver $primary_dns" > /etc/resolv.conf
+            [[ -n "$secondary_dns" ]] && echo -e "nameserver $secondary_dns" >> /etc/resolv.conf
+            echo -e "\n${GREEN}Custom DNS set to: $primary_dns ${secondary_dns:+and $secondary_dns}${WHITE}\n"
+            ;;
+        4)
+            echo -e "\n${YELLOW}Service Check Coming Soon${WHITE}\n"
+            ;;
+        5)
+            echo -e "One DNS is a tool to manage and enhance DNS configurations easily on Linux systems."
+            ;;
+        6)
+            echo -e "${GREEN}Current DNS settings:${WHITE}"
+            cat /etc/resolv.conf
+            ;;
+        00)
+            echo -e "\n${YELLOW}Goodbye!${WHITE}\n"
+            exit 0
+            ;;
+        *)
+            echo -e "${RED}Invalid option, please try again.${WHITE}\n"
+            ;;
+    esac
+done
